8f431d06c68409b1963fb52ab8828035a643d812
hopeaktian <hopeaktian@gmail.com>
2018-12-28 21:11:59 +0800

    git rm --cached config.py

--- app/config.py
+++
@@ -1,9 +0,0 @@
-class Config(object):
-    SECRET_KEY = ''
-
-class ProdConfig(Config):
-    pass
-
-class DevConfig(Config):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:YOURPASSWORD@120.79.139.82:3306/rooprint"
